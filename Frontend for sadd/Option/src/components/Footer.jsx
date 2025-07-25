bbe618dfc4407a48c708ea6dd881980a3a43e0d4
Saad Srabon <saadsrabon2@gmail.com>
2025-07-25 20:19:06 +0600

    files changes

--- Frontend for sadd/Option/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full bg-white shadow p-4 text-center text-gray-500 mt-auto">
-      © 2024 Comfea. All rights reserved.
-    </footer>
-  );
-} 
